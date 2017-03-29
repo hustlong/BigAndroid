4f811d575b82173d58253c5e3f7b7e3dffbd0b0d
biglong <biglongcc.gmail.com>
2017-03-30 00:11:22 +0800

    colors flow circle
    colors flow

---
+++ app/src/main/java/cc/biglong/bigandroid/activity/ColorsFlowCircleActivity.java
@@ -0,0 +1,19 @@
+package cc.biglong.bigandroid.activity;
+
+import android.os.Bundle;
+
+import cc.biglong.bigandroid.R;
+import cc.biglong.bigandroid.base.BaseActivity;
+
+/**
+ * Created by biglong on 2017/3/30.
+ */
+
+public class ColorsFlowCircleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_colors_flow_circle);
+    }
+}
